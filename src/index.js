513a2376de917e74e6fedf205b12204e92fd39ea
PeksiGrem <peksigrem@yandex.ru>
2021-03-15 13:59:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const config1 = ['()', '[]', '{}', '||', '12', '34', '56', '77', '88']
+    let tempStr = str.split('');
+    let res = tempStr;
+    for (let i = 0; i < tempStr.length - 1; i++) {
+
+        for (let j = 0; j < config1.length; j++) {
+            if (tempStr[i] === config1[j][0] && tempStr[i + 1] === config1[j][1]) {
+                res.splice(i, 2)
+                i = 0;
+                j = 0;
+                
+            }
+        }
+        
+    }
+    if(tempStr[0] === '(' && tempStr[1] === ')' && res.length === 2){
+        return true;
+    }
+    
+    return res.length === 0;
 }
